0967ebaef55044ad915635e0324d468f7a609ddf
Marco Cabrera <highlandsmac@yahoo.com>
2025-03-02 21:21:48 -0600

    update to info pages and deciding the redering for the json editor tool

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { JsonEditorComponent } from './json-editor/json-editor.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'editor', component: JsonEditorComponent },
+  { path: '**', redirectTo: 'about' }
+];
---
+++ src/app/json-editor/json-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-json-editor',
+  imports: [],
+  templateUrl: './json-editor.component.html',
+  styleUrl: './json-editor.component.css'
+})
+export class JsonEditorComponent {
+
+}
